f8b0d1acdf1cd9b943083b5709d903bb77252af7
Paul Crane <paul@acheronsoft.com>
2020-08-18 17:22:48 -0500

    Initial handling of a single directory(and below) size.

--- duble.go
+++ duble.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/alexflint/go-arg"
+	"github.com/dustin/go-humanize"
+)
+
+var appArgs struct {
+	DirName     string `arg:"positional"`
+	ListSubDirs bool   `arg:"-l, --list" default:"false" help:"List diretories under the passed directory"`
+}
+
+// TODO: Return a list of humanized strings for each directory
+func getDirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			size += info.Size()
+		}
+
+		return err
+	})
+
+	return size, err
+}
+
 func main() {
+	arg.MustParse(&appArgs)
+
+	if appArgs.DirName != "" {
+		if appArgs.ListSubDirs {
+			//listDirs()
+		} else {
+			dirSize, err := getDirSize(appArgs.DirName)
+
+			if err != nil {
+				fmt.Println("Error getting size of directory")
+			} else {
+				fmt.Println(humanize.Bytes(uint64(dirSize)))
+			}
+		}
+	} else {
+		fmt.Println("Using default")
+	}
 
 }
