eabd849045fc339890f8591fd83343c0f72e3c43
Paul Crane <paul@acheronsoft.com>
2022-11-07 21:35:10 -0600

    Remove TODO

--- cmd/duble/args.go
+++ cmd/duble/args.go
@@ -1,6 +1,6 @@
 package main
 
-type ProgramArgs struct { // TODO: Make ProgramArgs
+type ProgramArgs struct {
 	DirName           string `arg:"positional"`
 	ListRootFilesOnly bool   `arg:"-r, --root-only" default:"false" help:"List only the files in root directory/the passed directory name."`
 	ListAll           bool   `arg:"-a, --list-all" default:"false" help:"Additionally list hidden files and directories."`
