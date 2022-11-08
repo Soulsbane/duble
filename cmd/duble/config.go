8cf9f8dc979c1af938134ba443b6334f96184d4d
Paul Crane <paul@acheronsoft.com>
2022-11-07 21:36:26 -0600

    Add getConfigFilePath

--- cmd/duble/config.go
+++ cmd/duble/config.go
@@ -1,1 +1,18 @@
 package main
+
+import gap "github.com/muesli/go-app-paths"
+
+const (
+	vendor          string = "Raijinsoft"
+	applicationName string = "duble"
+)
+
+type Config struct {
+}
+
+func getConfigFilePath() (string, error) {
+	scope := gap.NewVendorScope(gap.User, vendor, applicationName)
+	path, err := scope.ConfigPath("config.toml")
+
+	return path, err
+}
