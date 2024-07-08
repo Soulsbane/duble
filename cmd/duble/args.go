844ea1a64eb35e4e57eef506bb34385bf9781889
Paul Crane <paul@acheronsoft.com>
2024-07-08 16:03:29 -0500

    Add initial functions for sorting output

--- cmd/duble/args.go
+++ cmd/duble/args.go
@@ -4,4 +4,5 @@ type ProgramArgs struct {
 	DirName           string `arg:"positional"`
 	ListRootFilesOnly bool   `arg:"-r, --root-only" default:"false" help:"List only the files in root directory/the passed directory name."`
 	ListAll           bool   `arg:"-a, --list-all" default:"false" help:"Additionally list hidden files and directories."`
+	SortBy            string `arg:"-s, --sort-by" default:"size" help:"Sort by size or name."`
 }
