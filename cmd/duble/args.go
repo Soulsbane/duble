7b7cbfef0c6ec96a873fc49d8de06d7a44e2697d
Paul Crane <paul@acheronsoft.com>
2024-07-09 10:06:04 -0500

    Add Descending variable to ProgramArgs

--- cmd/duble/args.go
+++ cmd/duble/args.go
@@ -5,4 +5,5 @@ type ProgramArgs struct {
 	ListRootFilesOnly bool   `arg:"-r, --root-only" default:"false" help:"List only the files in root directory/the passed directory name."`
 	ListAll           bool   `arg:"-a, --list-all" default:"false" help:"Additionally list hidden files and directories."`
 	SortBy            string `arg:"-s, --sort-by" default:"size" help:"Sort by size or name."`
+	Descending        bool   `arg:"-d, --descending" default:"false" help:"Sort in descending order."`
 }
